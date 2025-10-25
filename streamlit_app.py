67d6aaa02a461c5e612a20d4cc5a9f8070c1ac37
butusovpn496-gif <butusovpn496@gmail.com>
2025-10-25 18:04:19 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ vector_store = FAISS(
 from langchain import hub
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import START, StateGraph
-from typing_extensions import List, TypedDict
+# from langgraph.graph import START, StateGraph
+# from typing_extensions import List, TypedDict
 
 from langchain_community.document_loaders import PyPDFLoader
 
